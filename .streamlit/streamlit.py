dee49b33b5a6b1837499e50ad5f319b3b18ac631
Balma Agost <147596951+balagostr@users.noreply.github.com>
2024-02-01 13:18:27 +0100

    Update streamlit.py

--- .streamlit/streamlit.py
+++ .streamlit/streamlit.py
@@ -4,3 +4,22 @@ from google.cloud import bigquery
 import os
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/balmagostr/Documents/GitHub/CochePole/.streamlit/credentials.json"
+
+# Inicializa el cliente de BigQuery
+client = bigquery.Client()
+
+# Consulta los datos desde BigQuery
+query = """
+SELECT *
+FROM 
+"""
+
+# Ejecuta la consulta
+query_job = client.query(query)
+
+# Muestra los resultados en Streamlit
+st.write("Datos de BigQuery")
+for row in query_job:
+    st.write(row)
+
+
