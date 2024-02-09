7727a80bc5f0e84c81137d35dfb4b2caf41c85ff
hugofmr <130927244+hugofmr@users.noreply.github.com>
2024-02-10 00:24:49 +0100

    [UPDATED] main.py
     actualizacion y puesta a prueba, YA FUNCIONAAAAAAAAAAAAAAAA lptm
    
    solo se lee por consola y matchea.  no escribe en BigQuery, ese es el siguiente paso.

--- df_v1/main.py
+++ df_v1/main.py
@@ -19,74 +19,80 @@ bucket_name = "test-dp2"
 
 # Funcion para dar formato
 
-def decode_message(msg):
+# def decode_message(msg):
 
-    output = msg.decode('utf-8')
+#     output = msg.decode('utf-8')
 
-    logging.info("New PubSub Message: %s", output)
+#     logging.info("New PubSub Message: %s", output)
 
-    return json.loads(output)
+#     return json.loads(output)
+
+def decode_json(element):
+    return json.loads(element)
+
+def run():
+    with beam.Pipeline(options=PipelineOptions(
+        streaming=True,
+        project=project_id,
+        runner="DirectRunner"
+    )) as p:
+        # Lectura de mensajes de vehiculos desde PubSub
+        vehicles = (
+            p | "ReadFromPubSubViajes" >> ReadFromPubSub(subscription=f'projects/{project_id}/subscriptions/{subscription_name_viajes}')
+              | "DecodeVehicles" >> beam.Map(decode_json)
+              | "WindowViajes" >> beam.WindowInto(FixedWindows(10))  # 10 segundos de ventana
+              | 'PairVehicles' >> beam.Map(lambda v: (v['viaje_id'], v))              
+        )
+        
+        # Lectura de mensaje de clientes desde PubSub
+        users = (
+            p | "ReadFromPubSubClientes" >> ReadFromPubSub(subscription=f'projects/{project_id}/subscriptions/{subscription_name_clientes}')
+              | "DecodeClientes" >> beam.Map(decode_json)
+              | "WindowClientes" >> beam.WindowInto(FixedWindows(10))  # 10 segundos de ventana
+              | 'PairUsers' >> beam.Map(lambda u: (u['viaje_id'], u))             
+        )
+        
+        # Combinacion de las coleciones de vehiculos y usuarios
+        vehicles_and_users = (
+            {'vehicles': vehicles, 'users': users} 
+            | 'CombineCollections' >> beam.CoGroupByKey()
+        )
+
+        # Procesamiento de las coincidencias entre vehiculos y usuarios
+        matches = (
+            vehicles_and_users
+            | 'MatchVehiclesAndUsers' >> beam.ParDo(MatchVehiclesAndUsersDoFn())
+            | 'PrintMatches' >> beam.Map(print)
+        )
+
 
 
 # def run():
-#     with beam.Pipeline(options=PipelineOptions(
-#         streaming=True,
-#         project=project_id,
-#         runner="DirectRunner"
-#     )) as p:
-#         vehicles = (
-#             p | "ReadFromPubSubViajes" >> ReadFromPubSub(subscription=f'projects/{project_id}/subscriptions/{subscription_name_viajes}')
-#               | "DecodeViajes" >> beam.Map(decode_message)
-#               | 'PairVehicles' >> beam.Map(lambda v: (v['viaje_id'], v))
-#               | "WindowViajes" >> beam.WindowInto(FixedWindows(10))  # 10 segundos de ventana
-#         )
+#     vehicles_data = [
+#         {"vehicle_id": 125, "viaje_id": 23, "latitud": 39.46829, "longitud": -0.36069, "num_plazas": 2},
+#         {"vehicle_id": 126, "viaje_id": 24, "latitud": 39.46830, "longitud": -0.36068, "num_plazas": 2}
+#     ]
+#     users_data = [
+#         {"cliente_id": 33, "viaje_id": 245, "latitud": 39.46827, "longitud": -0.36069},
+#         {"cliente_id": 34, "viaje_id": 24, "latitud": 39.46830, "longitud": -0.36068}
+#     ]
 
-#         users = (
-#             p | "ReadFromPubSubClientes" >> ReadFromPubSub(subscription=f'projects/{project_id}/subscriptions/{subscription_name_clientes}')
-#               | "DecodeClientes" >> beam.Map(decode_message)
-#               | 'PairUsers' >> beam.Map(lambda u: (u['viaje_id'], u))
-#               | "WindowClientes" >> beam.WindowInto(FixedWindows(10))  # 10 segundos de ventana
-#         )
-        
-#         vehicles_and_users = (
-#             {'vehicles': vehicles, 'users': users} 
-#             | 'CombineCollections' >> beam.CoGroupByKey()
-#         )
+#     # Definición del pipeline
+#     with beam.Pipeline() as p:
+#         # Datos de ejemplo
+#         vehicles = (p
+#                     | 'CreateVehicles' >> beam.Create(vehicles_data)
+#                     | 'PairVehicles' >> beam.Map(lambda v: (v['viaje_id'], v)))
+#         users = (p
+#                  | 'CreateUsers' >> beam.Create(users_data)
+#                  | 'PairUsers' >> beam.Map(lambda u: (u['viaje_id'], u)))
 
-#         matches = (
-#             vehicles_and_users
-#             | 'MatchVehiclesAndUsers' >> beam.ParDo(MatchVehiclesAndUsersDoFn())
-#             | 'PrintMatches' >> beam.Map(print)
-#         )
+#         vehicles_and_users = ({'vehicles': vehicles, 'users': users} 
+#                               | 'CombineCollections' >> beam.CoGroupByKey())
 
-
-
-def run():
-    vehicles_data = [
-        {"vehicle_id": 125, "viaje_id": 23, "latitud": 39.46829, "longitud": -0.36069, "num_plazas": 2},
-        {"vehicle_id": 126, "viaje_id": 24, "latitud": 39.46830, "longitud": -0.36068, "num_plazas": 2}
-    ]
-    users_data = [
-        {"cliente_id": 33, "viaje_id": 245, "latitud": 39.46827, "longitud": -0.36069},
-        {"cliente_id": 34, "viaje_id": 24, "latitud": 39.46830, "longitud": -0.36068}
-    ]
-
-    # Definición del pipeline
-    with beam.Pipeline() as p:
-        # Datos de ejemplo
-        vehicles = (p
-                    | 'CreateVehicles' >> beam.Create(vehicles_data)
-                    | 'PairVehicles' >> beam.Map(lambda v: (v['viaje_id'], v)))
-        users = (p
-                 | 'CreateUsers' >> beam.Create(users_data)
-                 | 'PairUsers' >> beam.Map(lambda u: (u['viaje_id'], u)))
-
-        vehicles_and_users = ({'vehicles': vehicles, 'users': users} 
-                              | 'CombineCollections' >> beam.CoGroupByKey())
-
-        matches = (vehicles_and_users
-                   | 'MatchVehiclesAndUsers' >> beam.ParDo(MatchVehiclesAndUsersDoFn())
-                   | 'PrintMatches' >> beam.Map(print))
+#         matches = (vehicles_and_users
+#                    | 'MatchVehiclesAndUsers' >> beam.ParDo(MatchVehiclesAndUsersDoFn())
+#                    | 'PrintMatches' >> beam.Map(print))
 
 
 if __name__ == '__main__':
