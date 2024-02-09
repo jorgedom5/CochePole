1d19ce88f45ad653aa8dddfe5e7fb6b2af65e0ca
hugofmr <130927244+hugofmr@users.noreply.github.com>
2024-02-10 00:07:08 +0100

    Update main.py

--- df_v1/main.py
+++ df_v1/main.py
@@ -28,65 +28,65 @@ def decode_message(msg):
     return json.loads(output)
 
 
-def run():
-    with beam.Pipeline(options=PipelineOptions(
-        streaming=True,
-        project=project_id,
-        runner="DirectRunner"
-    )) as p:
-        vehicles = (
-            p | "ReadFromPubSubViajes" >> ReadFromPubSub(subscription=f'projects/{project_id}/subscriptions/{subscription_name_viajes}')
-              | "DecodeViajes" >> beam.Map(decode_message)
-              | 'PairVehicles' >> beam.Map(lambda v: (v['viaje_id'], v))
-              #| "WindowViajes" >> beam.WindowInto(FixedWindows(10))  # 10 segundos de ventana
-        )
-
-        users = (
-            p | "ReadFromPubSubClientes" >> ReadFromPubSub(subscription=f'projects/{project_id}/subscriptions/{subscription_name_clientes}')
-              | "DecodeClientes" >> beam.Map(decode_message)
-              | 'PairUsers' >> beam.Map(lambda u: (u['viaje_id'], u))
-              #| "WindowClientes" >> beam.WindowInto(FixedWindows(10))  # 10 segundos de ventana
-        )
-        
-        vehicles_and_users = (
-            {'vehicles': vehicles, 'users': users} 
-            | 'CombineCollections' >> beam.CoGroupByKey()
-        )
-
-        matches = (
-            vehicles_and_users
-            | 'MatchVehiclesAndUsers' >> beam.ParDo(MatchVehiclesAndUsersDoFn())
-            | 'PrintMatches' >> beam.Map(print)
-        )
-
-
-
 # def run():
-#     vehicles_data = [
-#         {"vehicle_id": 125, "viaje_id": 23, "latitud": 39.46829, "longitud": -0.36069, "num_plazas": 2},
-#         {"vehicle_id": 126, "viaje_id": 24, "latitud": 39.46830, "longitud": -0.36068, "num_plazas": 2}
-#     ]
-#     users_data = [
-#         {"cliente_id": 33, "viaje_id": 245, "latitud": 39.46827, "longitud": -0.36069},
-#         {"cliente_id": 34, "viaje_id": 24, "latitud": 39.46830, "longitud": -0.36068}
-#     ]
+#     with beam.Pipeline(options=PipelineOptions(
+#         streaming=True,
+#         project=project_id,
+#         runner="DirectRunner"
+#     )) as p:
+#         vehicles = (
+#             p | "ReadFromPubSubViajes" >> ReadFromPubSub(subscription=f'projects/{project_id}/subscriptions/{subscription_name_viajes}')
+#               | "DecodeViajes" >> beam.Map(decode_message)
+#               | 'PairVehicles' >> beam.Map(lambda v: (v['viaje_id'], v))
+#               | "WindowViajes" >> beam.WindowInto(FixedWindows(10))  # 10 segundos de ventana
+#         )
 
-#     # Definición del pipeline
-#     with beam.Pipeline() as p:
-#         # Datos de ejemplo
-#         vehicles = (p
-#                     | 'CreateVehicles' >> beam.Create(vehicles_data)
-#                     | 'PairVehicles' >> beam.Map(lambda v: (v['viaje_id'], v)))
-#         users = (p
-#                  | 'CreateUsers' >> beam.Create(users_data)
-#                  | 'PairUsers' >> beam.Map(lambda u: (u['viaje_id'], u)))
+#         users = (
+#             p | "ReadFromPubSubClientes" >> ReadFromPubSub(subscription=f'projects/{project_id}/subscriptions/{subscription_name_clientes}')
+#               | "DecodeClientes" >> beam.Map(decode_message)
+#               | 'PairUsers' >> beam.Map(lambda u: (u['viaje_id'], u))
+#               | "WindowClientes" >> beam.WindowInto(FixedWindows(10))  # 10 segundos de ventana
+#         )
+        
+#         vehicles_and_users = (
+#             {'vehicles': vehicles, 'users': users} 
+#             | 'CombineCollections' >> beam.CoGroupByKey()
+#         )
 
-        # vehicles_and_users = ({'vehicles': vehicles, 'users': users} 
-        #                       | 'CombineCollections' >> beam.CoGroupByKey())
+#         matches = (
+#             vehicles_and_users
+#             | 'MatchVehiclesAndUsers' >> beam.ParDo(MatchVehiclesAndUsersDoFn())
+#             | 'PrintMatches' >> beam.Map(print)
+#         )
 
-        # matches = (vehicles_and_users
-        #            | 'MatchVehiclesAndUsers' >> beam.ParDo(MatchVehiclesAndUsersDoFn())
-        #            | 'PrintMatches' >> beam.Map(print))
+
+
+def run():
+    vehicles_data = [
+        {"vehicle_id": 125, "viaje_id": 23, "latitud": 39.46829, "longitud": -0.36069, "num_plazas": 2},
+        {"vehicle_id": 126, "viaje_id": 24, "latitud": 39.46830, "longitud": -0.36068, "num_plazas": 2}
+    ]
+    users_data = [
+        {"cliente_id": 33, "viaje_id": 245, "latitud": 39.46827, "longitud": -0.36069},
+        {"cliente_id": 34, "viaje_id": 24, "latitud": 39.46830, "longitud": -0.36068}
+    ]
+
+    # Definición del pipeline
+    with beam.Pipeline() as p:
+        # Datos de ejemplo
+        vehicles = (p
+                    | 'CreateVehicles' >> beam.Create(vehicles_data)
+                    | 'PairVehicles' >> beam.Map(lambda v: (v['viaje_id'], v)))
+        users = (p
+                 | 'CreateUsers' >> beam.Create(users_data)
+                 | 'PairUsers' >> beam.Map(lambda u: (u['viaje_id'], u)))
+
+        vehicles_and_users = ({'vehicles': vehicles, 'users': users} 
+                              | 'CombineCollections' >> beam.CoGroupByKey())
+
+        matches = (vehicles_and_users
+                   | 'MatchVehiclesAndUsers' >> beam.ParDo(MatchVehiclesAndUsersDoFn())
+                   | 'PrintMatches' >> beam.Map(print))
 
 
 if __name__ == '__main__':
