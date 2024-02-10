efc1c898cd8e503285f4f672414759e10b0030bf
jorgedom5 <148787367+jorgedom5@users.noreply.github.com>
2024-02-10 13:31:41 +0100

    [Changed] Old Api is back

--- api/app.py
+++ api/app.py
@@ -1,13 +1,7 @@
 from flask import Flask, render_template, jsonify
-from concurrent.futures import ThreadPoolExecutor
 from subprocess import Popen
 
 app = Flask(__name__)
-executor = ThreadPoolExecutor()
-
-def run_process(command):
-    process = Popen(command)
-    process.wait()
 
 @app.route('/')
 def index():
@@ -15,16 +9,12 @@ def index():
 
 @app.route('/generate_vehicle_data', methods=['POST'])
 def generate_vehicle_data():
-    commands = [['python3', 'generator_vehicle.py', '--project_id', 'dataproject-2-413010', '--topic_name', 'dp2_viajes']] * 10
-    with executor as e:
-        e.map(run_process, commands)
+    process = Popen(['python3', 'generator_vehicle.py', '--project_id', 'dataproject-2-413010', '--topic_name', 'dp2_viajes'])
     return jsonify({"status": "success", "message": "Generating Vehicles."})
 
 @app.route('/generate_user_data', methods=['POST'])
 def generate_user_data():
-    commands = [['python3', 'generador_usuarios.py', '--project_id', 'dataproject-2-413010', '--topic_name', 'dp2_clientes']] * 10
-    with executor as e:
-        e.map(run_process, commands)
+    process = Popen(['python3', 'generador_usuarios.py', '--project_id', 'dataproject-2-413010', '--topic_name', 'dp2_clientes'])
     return jsonify({"status": "success", "message": "Generating Users."})
 
 if __name__ == '__main__':
