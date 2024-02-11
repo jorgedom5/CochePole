e7da4e57016a75bf8fea6d62a48a2f3346668194
hugofmr <130927244+hugofmr@users.noreply.github.com>
2024-02-11 11:46:15 +0100

    [UPDATED] MatchVehiclesAndUsersDoFn modificado y actualizado
    Se ha modificado la funciones para que los usuarios puedan subir a un vehicle en funcion de las plazas que se tienen disponibles.
    
    Tambien se ha creado filtro para que una vez haya sido utilizado un cliente, se lo ignore en futuro match con el mismo viaje_id

--- df_v1/Funciones.py
+++ df_v1/Funciones.py
@@ -11,12 +11,12 @@ def calculate_distance(lat1, lon1, lat2, lon2):
          math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) *
          math.sin(dLon/2) * math.sin(dLon/2))
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
-    return R * c
+    return R * c * 1000 # Multiplicar por 1000 para convertir a metros
 
 # Función para verificar si la ubicación del usuario está dentro del rango de la ruta del vehículo, depende de "calculate_distance"
 def is_within_route(user_location, vehicle_route):
     for point in vehicle_route:
-        if calculate_distance(*user_location, *point) <= 5:  # Dentro de 0,35 km, lo que es lo mismo a unos 3 mins andando.
+        if calculate_distance(*user_location, *point) <= 20:  # Dentro de 20 metros, lo que es lo mismo a unos 3 mins andando.
             return True
     return False
 
@@ -27,6 +27,12 @@ class MatchVehiclesAndUsersDoFn(beam.DoFn):
         
         viaje_id, collections = element  # Desempaquetar la tupla
         vehicles, users = collections['vehicles'], collections['users'] # Separar las listas de vehículos y usuarios de las collections 
+
+        # Registro de usuarios asignados
+
+        clientes_emparejados = set()
+
+
 
 
         # Iterar sobre cada vehículo en la lista de vehículos
@@ -49,19 +55,25 @@ class MatchVehiclesAndUsersDoFn(beam.DoFn):
                     longitud=user['longitud']
                 )
 
+                # Se verifica si el cliente ya ha sido emparejado SUGERENCIA
+                if cliente_obj.cliente_id in clientes_emparejados:
+                    continue  # Si tiene viaje, lo salta.
+
+
+
                 # Definir las ubicaciones de usuario y vehículo
                 user_location = (cliente_obj.latitud, cliente_obj.longitud)
                 vehicle_location = (vehicle_obj.latitud, vehicle_obj.longitud)
-
-                # Comprobar si el vehículo está disponible, si el viaje_id coincide y si el usuario está en la ruta del vehículo
-                if (vehicle_obj.is_available and 
+            
+                if (vehicle_obj.is_available() and  
                     vehicle_obj.viaje_id == cliente_obj.viaje_id and 
-                    is_within_route(user_location, [vehicle_location])):  # Asegurándose de que vehicle_route sea una lista
-                    
-                     # Iniciar el viaje con el usuario y generar el resultado
+                    is_within_route(user_location, [vehicle_location])):
+
                     vehicle_obj.start_journey_with(cliente_obj.cliente_id)
                     yield {'user_id': cliente_obj.cliente_id, 'vehicle_id': vehicle_obj.vehicle_id, 'viaje_id': vehicle_obj.viaje_id, 'latitud':vehicle_obj.latitud, 'longitud':vehicle_obj.longitud}
-                    break # Romper el bucle interno una vez que se encuentra una coincidencia para que no busque más clientes
+
+            if not vehicle_obj.is_available():
+                print(f"Vehículo con ID {vehicle_obj.vehicle_id} en el viaje con ID {viaje_id} está lleno.")
             else:
                 print(f"No hay clientes para el vehículo con ID {vehicle_obj.vehicle_id} en el viaje con ID {viaje_id}")
                     
