68c08e3f468c5d79ee3fa800b9401f33a06eb2e8
jorgedom5 <148787367+jorgedom5@users.noreply.github.com>
2024-02-17 13:23:21 +0100

    Update graficos.py

--- streamlit/graficos.py
+++ streamlit/graficos.py
@@ -4,108 +4,63 @@ from folium.plugins import HeatMap
 import pandas as pd
 import matplotlib.pyplot as plt
 from google.cloud import bigquery
+import plotly.express as px
 
 # Crear cliente de BigQuery
 client = bigquery.Client(project='dataproject-2-413010')
 
 ## GRÁFICO DE RECAUDACIÓN POR CONDUCTOR
 
-# Consulta SQL para calcular la recaudación por conductor
-query_recaudacion = """
-SELECT vehiculo_id, SUM(pago_viaje) AS total_recaudado
-FROM `dataproject-2-413010.BBDD.CochePole_BD`
-GROUP BY vehiculo_id
+query_df = """
+SELECT p.*, 
+v.nombre_conductor, v.marca_coche, v.color_coche, v.anio_fabricacion, 
+v.tipo_combustible, v.tiene_calefaccion, v.puntos_carnet, v.cilindraje_motor, v.kilometraje,
+c.nombre_cliente, c.apellido_cliente, c.edad_cliente, c.genero_cliente,
+FROM dataproject-2-413010.BBDD.CochePole_BD p
+left join dataproject-2-413010.BBDD.tabla_vehiculos v on p.vehiculo_id = v.vehiculo_id
+left join dataproject-2-413010.BBDD.tabla_clientes c on p.cliente_id = c.cliente_id
 """
 
-# Ejecutar la consulta y obtener los resultados
-recaudacion_df = client.query(query_recaudacion).to_dataframe()
+df = client.query(query_df).to_dataframe()
 
-# Visualización en Streamlit
+# GRÁFICO DE RECAUDACIÓN POR CONDUCTOR
+recaudacion_df = df.groupby('nombre_conductor')['pago_viaje'].sum().reset_index()
+
 st.title('Recaudación por Conductor al Final del Día')
-st.bar_chart(recaudacion_df.set_index('vehiculo_id'))
+fig_recaudacion = px.bar(recaudacion_df, x='nombre_conductor', y='pago_viaje', labels={'pago_viaje': 'Recaudación'})
+st.plotly_chart(fig_recaudacion)
 
-## GRÁFICO DE Nº VIAJES POR CLIENTE
+# GRÁFICO DE Nº VIAJES POR CLIENTE
+viajes_por_cliente_df = df['cliente_id'].value_counts().reset_index()
+viajes_por_cliente_df.columns = ['cliente_id', 'num_viajes']
 
-# Consulta SQL para contar el número de viajes por cliente
-query_viajes_por_cliente = """
-SELECT cliente_id, COUNT(*) AS num_viajes
-FROM `dataproject-2-413010.BBDD.CochePole_BD`
-GROUP BY cliente_id
-"""
-
-# Ejecutar la consulta y obtener los resultados
-viajes_por_cliente_df = client.query(query_viajes_por_cliente).to_dataframe()
-
-# Visualización en Streamlit
 st.title('Número de Viajes Realizados por Cliente')
-st.bar_chart(viajes_por_cliente_df.set_index('cliente_id'))
+fig_viajes_por_cliente = px.bar(viajes_por_cliente_df, x='num_viajes', y='cliente_id', labels={'num_viajes': 'Número de Viajes', 'cliente_id': 'ID del Cliente'})
+st.plotly_chart(fig_viajes_por_cliente)
 
-## GRÁFICO DE Nº VIAJES POR CONDUCTOR
+# GRÁFICO DE Nº VIAJES POR CONDUCTOR
+viajes_por_conductor_df = df['vehiculo_id'].value_counts().reset_index()
+viajes_por_conductor_df.columns = ['vehiculo_id', 'num_viajes']
 
-# Consulta SQL para contar el número de viajes por conductor
-query_viajes_por_conductor = """
-SELECT vehiculo_id, COUNT(*) AS num_viajes
-FROM `dataproject-2-413010.BBDD.CochePole_BD`
-GROUP BY vehiculo_id
-ORDER BY num_viajes DESC
-"""
-
-# Ejecutar la consulta y obtener los resultados
-viajes_por_conductor_df = client.query(query_viajes_por_conductor).to_dataframe()
-
-# Visualización en Streamlit
 st.title('Número de Viajes Realizados por Conductor')
-st.bar_chart(viajes_por_conductor_df.set_index('vehiculo_id'))
+fig_viajes_por_conductor = px.bar(viajes_por_conductor_df, x='num_viajes', y='vehiculo_id', labels={'num_viajes': 'Número de Viajes', 'vehiculo_id': 'ID del Conductor'})
+st.plotly_chart(fig_viajes_por_conductor)
 
-# También puedes mostrar los datos en forma de tabla si lo deseas
-st.subheader('Detalle de Viajes por Conductor')
-st.write(viajes_por_conductor_df)
-
-## GRÁFICO METODO DE PAGO MÁS UTILIZADO
-
-# Query Método Pago
-
-query_metodo_pago = """
-SELECT 
-    metodo_pago,
-    COUNT(*) AS cantidad,
-    ROUND(COUNT(*) * 100.0 / (SELECT COUNT(*) FROM `dataproject-2-413010.BBDD.CochePole_BD`), 2) AS porcentaje
-FROM 
-    `dataproject-2-413010.BBDD.CochePole_BD`
-GROUP BY 
-    metodo_pago
-"""
-
-metodo_pago_preferido_df = client.query(query_metodo_pago).to_dataframe()
+# GRÁFICO METODO DE PAGO MÁS UTILIZADO
+metodo_pago_preferido_df = df['metodo_pago'].value_counts().reset_index()
+metodo_pago_preferido_df.columns = ['metodo_pago', 'cantidad']
+metodo_pago_preferido_df['porcentaje'] = metodo_pago_preferido_df['cantidad'] * 100.0 / len(df)
 
 st.title('Método de pago preferido por los clientes')
+fig_metodo_pago = px.pie(metodo_pago_preferido_df, names='metodo_pago', values='cantidad', labels={'cantidad': 'Cantidad'}, title='Método de Pago Preferido')
+st.plotly_chart(fig_metodo_pago)
 
-# Gráfico de pastel
-fig, ax = plt.subplots()
-metodo_pago_preferido_df.set_index('metodo_pago')['cantidad'].plot(kind='pie', autopct='%1.1f%%', ax=ax)
-ax.set_ylabel('')  # Elimina la etiqueta del eje y
+rating_medio_por_vehiculo_df = df.groupby('vehiculo_id')['rating'].mean().reset_index()
+rating_medio_por_vehiculo_df.columns = ['vehiculo_id', 'puntuacion_promedio']
 
-st.pyplot(fig)
-
-# Query Rating
-query_rating_medio_por_vehiculo = """
-SELECT vehiculo_id, AVG(rating) as puntuacion_promedio
-FROM `dataproject-2-413010.BBDD.CochePole_BD`
-GROUP BY vehiculo_id
-"""
-
-rating_medio_por_vehiculo_df = client.query(query_rating_medio_por_vehiculo).to_dataframe()
-
-# Título del Gráfico
 st.title('Nota Promedia de Nuestros Conductores')
-
-# Histograma de los ratings promedio por vehículo
-fig, ax = plt.subplots()
-ax.hist(rating_medio_por_vehiculo_df['puntuacion_promedio'], bins=5, color='skyblue', edgecolor='black')
-ax.set_xlabel('Rating Promedio')
-ax.set_ylabel('Frecuencia')
-ax.set_title('Histograma de Ratings Promedio por Vehículo')
-st.pyplot(fig)
+fig_rating_promedio = px.histogram(rating_medio_por_vehiculo_df, x='puntuacion_promedio', nbins=5, labels={'puntuacion_promedio': 'Rating Promedio'}, title='Histograma de Ratings Promedio por Vehículo')
+st.plotly_chart(fig_rating_promedio)
 
 # ## GRÁFICO MAPA DE CALOR DE COORDENADAS
 
