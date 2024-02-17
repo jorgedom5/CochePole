5f5d0a9a8a18332b4dc5dea7794e4b96c91d424e
cbuenrostrovalverde <148787241+cbuenrostrovalverde@users.noreply.github.com>
2024-02-17 12:56:09 +0100

    [UPDATE] Gráficos
    Traemos los cambios de una rama a esta.

--- streamlit/graficos.py
+++ streamlit/graficos.py
@@ -13,7 +13,7 @@ client = bigquery.Client(project='dataproject-2-413010')
 # Consulta SQL para calcular la recaudación por conductor
 query_recaudacion = """
 SELECT vehiculo_id, SUM(pago_viaje) AS total_recaudado
-FROM `dataproject-2-413010.BBDD.simulador_streamlit2`
+FROM `dataproject-2-413010.BBDD.CochePole_BD`
 GROUP BY vehiculo_id
 """
 
@@ -29,7 +29,7 @@ st.bar_chart(recaudacion_df.set_index('vehiculo_id'))
 # Consulta SQL para contar el número de viajes por cliente
 query_viajes_por_cliente = """
 SELECT cliente_id, COUNT(*) AS num_viajes
-FROM `dataproject-2-413010.BBDD.simulador_streamlit2`
+FROM `dataproject-2-413010.BBDD.CochePole_BD`
 GROUP BY cliente_id
 """
 
@@ -45,8 +45,9 @@ st.bar_chart(viajes_por_cliente_df.set_index('cliente_id'))
 # Consulta SQL para contar el número de viajes por conductor
 query_viajes_por_conductor = """
 SELECT vehiculo_id, COUNT(*) AS num_viajes
-FROM `dataproject-2-413010.BBDD.simulador_streamlit2`
+FROM `dataproject-2-413010.BBDD.CochePole_BD`
 GROUP BY vehiculo_id
+ORDER BY num_viajes DESC
 """
 
 # Ejecutar la consulta y obtener los resultados
@@ -65,9 +66,14 @@ st.write(viajes_por_conductor_df)
 # Query Método Pago
 
 query_metodo_pago = """
-SELECT metodo_pago as metodo_pago
-FROM `dataproject-2-413010.BBDD.simulador_streamlit2`
-GROUP BY metodo_pago
+SELECT 
+    metodo_pago,
+    COUNT(*) AS cantidad,
+    ROUND(COUNT(*) * 100.0 / (SELECT COUNT(*) FROM `dataproject-2-413010.BBDD.CochePole_BD`), 2) AS porcentaje
+FROM 
+    `dataproject-2-413010.BBDD.CochePole_BD`
+GROUP BY 
+    metodo_pago
 """
 
 metodo_pago_preferido_df = client.query(query_metodo_pago).to_dataframe()
@@ -76,7 +82,7 @@ st.title('Método de pago preferido por los clientes')
 
 # Gráfico de pastel
 fig, ax = plt.subplots()
-metodo_pago_preferido_df['metodo_pago'].value_counts().plot(kind='pie', autopct='%1.1f%%', ax=ax)
+metodo_pago_preferido_df.set_index('metodo_pago')['cantidad'].plot(kind='pie', autopct='%1.1f%%', ax=ax)
 ax.set_ylabel('')  # Elimina la etiqueta del eje y
 
 st.pyplot(fig)
@@ -84,14 +90,14 @@ st.pyplot(fig)
 # Query Rating
 query_rating_medio_por_vehiculo = """
 SELECT vehiculo_id, AVG(rating) as puntuacion_promedio
-FROM `dataproject-2-413010.BBDD.simulador_streamlit2`
+FROM `dataproject-2-413010.BBDD.CochePole_BD`
 GROUP BY vehiculo_id
 """
 
 rating_medio_por_vehiculo_df = client.query(query_rating_medio_por_vehiculo).to_dataframe()
 
 # Título del Gráfico
-st.title('Histograma de Ratings Promedio por Vehículo')
+st.title('Nota Promedia de Nuestros Conductores')
 
 # Histograma de los ratings promedio por vehículo
 fig, ax = plt.subplots()
@@ -103,26 +109,26 @@ st.pyplot(fig)
 
 # ## GRÁFICO MAPA DE CALOR DE COORDENADAS
 
-query_coordenadas_viaje = """
-SELECT latitud, longitud
-FROM `dataproject-2-413010.BBDD.simulador_streamlit2`
-"""
+# query_coordenadas_viaje = """
+# SELECT latitud, longitud
+# FROM `dataproject-2-413010.BBDD.CochePole_BD`
+# """
 
-# Ejecutar la consulta y obtener los resultados
-resultados = client.query(query_coordenadas_viaje).result()
+# # Ejecutar la consulta y obtener los resultados
+# resultados = client.query(query_coordenadas_viaje).result()
 
-# Crear un mapa centrado en un punto promedio de las coordenadas
-m = folium.Map(location=[0, 0], zoom_start=2)
+# # Crear un mapa centrado en un punto promedio de las coordenadas
+# m = folium.Map(location=[0, 0], zoom_start=2)
 
-# Agrupar las coordenadas de los viajes en una lista
-heat_data = [[row['latitud'], row['longitud']] for row in resultados]
+# # Agrupar las coordenadas de los viajes en una lista
+# heat_data = [[row['latitud'], row['longitud']] for row in resultados]
 
-# Agregar el mapa de calor al mapa
-HeatMap(heat_data).add_to(m)
+# # Agregar el mapa de calor al mapa
+# HeatMap(heat_data).add_to(m)
 
-# Guardar el mapa como HTML en una ruta específica
-html_file_path = "heatmap.html"
-m.save(html_file_path)
+# # Guardar el mapa como HTML en una ruta específica
+# html_file_path = "heatmap.html"
+# m.save(html_file_path)
 
-# Mostrar el mapa en Streamlit usando un iframe
-st.write('<iframe src="{}" width="100%" height="500"></iframe>'.format(html_file_path), unsafe_allow_html=True)
+# # Mostrar el mapa en Streamlit usando un iframe
+# st.write('<iframe src="{}" width="100%" height="500"></iframe>'.format(html_file_path), unsafe_allow_html=True)
